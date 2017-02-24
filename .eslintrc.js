bcd11896fbe36a6ddb3f8dcb4677ae1b3738cb71
sunkeke <kekesun@linkdoc.com>
2017-02-24 11:03:18 +0800

    skk

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,16 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 允许匿名new对象
+    'no-new': 0,
+    "no-unused-vars": [1, { "vars": "all", "args": "none" }],
+    "no-useless-constructor": 0,
+    "no-trailing-spaces": 0,
+    "padded-blocks": 0,
+    "one-var": 0,
+    "object-property-newline": 0,
+    // else 是否可以换行
+    "brace-style": 0
   }
 }
